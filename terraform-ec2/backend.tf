c9fec5c1039a495bee39326f160caad42daeb162
KrishnaPathak824 <pathakkrishna824@gmail.com>
2025-07-14 12:59:51 -0400

    Changes in the ec2-instance

--- terraform-ec2/backend.tf
+++ terraform-ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "krishna-tf-s3bucket"
+    bucket = "com.krishna-tf-s3bucket"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
   }
--- terraform-s3bucket/backend.tf
+++ terraform-s3bucket/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "krishna-tf-s3bucket"
+    bucket = "com.krishna-tf-s3bucket"
     key    = "s3/terraform.tfstate"
     region = "us-east-1"
   }
